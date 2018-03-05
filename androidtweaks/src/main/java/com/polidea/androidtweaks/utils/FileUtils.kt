0a3e146a046d6a7b034d4cb8540ce51f8aa4093c
Marta Woldańska <marta.woldanska@polidea.com>
2018-03-05 17:25:45 +0100

    Some tests added

--- androidtweaks/src/main/java/com/polidea/androidtweaks/utils/FileUtils.kt
+++ androidtweaks/src/main/java/com/polidea/androidtweaks/utils/FileUtils.kt
@@ -10,16 +10,17 @@ import java.io.FileWriter
 
 
 class FileUtils(context: Context) {
-    private val savedTweaksFilePath = context.filesDir.path + File.separator + "savedTweaks.yml"
+    var savedTweaksFilePath = context.filesDir.path + File.separator + "savedTweaks.yml"
+    var tweaksManager = TweaksManager.getInstance()
+    private val loaderOptions = LoaderOptions()
+    val yaml: Yaml = Yaml(loaderOptions)
 
     fun saveTweaksAsYaml() {
-        val loaderOptions = LoaderOptions()
         loaderOptions.isAllowDuplicateKeys = false
-        val yaml = Yaml(loaderOptions)
         val fileWriter = FileWriter(savedTweaksFilePath)
 
         val data: LinkedHashMap<String, Any> = LinkedHashMap()
-        TweaksManager.getInstance().params.forEach {
+        tweaksManager.params.forEach {
             data[it.name] = it.value
         }
 
@@ -32,15 +33,12 @@ class FileUtils(context: Context) {
         }
 
         val savedTweaks = File(savedTweaksFilePath)
-        System.out.println(savedTweaks.readText())
-
-        val yaml = Yaml()
         val list: Map<String, Any> = yaml.load(savedTweaks.bufferedReader().use {
             it.readText()
         })
 
         list.forEach {
-            TweaksManager.getInstance().addParam(TweakParam(it.key, it.value.javaClass.kotlin, it.value))
+            tweaksManager.addParam(TweakParam(it.key, it.value.javaClass.kotlin, it.value))
         }
     }
 }
