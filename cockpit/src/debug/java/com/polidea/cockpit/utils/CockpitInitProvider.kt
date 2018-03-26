0fdf9d5dbbbfc88ba7bd474a700669f9617eb955
Wojciech Piwonski <wojciech.piwonski@polidea.com>
2018-03-26 14:56:51 +0200

    Initialization moved to TweaksInitProvider

---
+++ cockpit/src/debug/java/com/polidea/cockpit/utils/CockpitInitProvider.kt
@@ -0,0 +1,34 @@
+package com.polidea.cockpit.utils
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+
+class CockpitInitProvider : ContentProvider() {
+
+    override fun onCreate(): Boolean {
+        FileUtils(context).readCockpitFromFile()
+        return false
+    }
+
+    override fun insert(uri: Uri?, values: ContentValues?): Uri? {
+        return null
+    }
+
+    override fun query(uri: Uri?, projection: Array<out String>?, selection: String?, selectionArgs: Array<out String>?, sortOrder: String?): Cursor? {
+        return null
+    }
+
+    override fun update(uri: Uri?, values: ContentValues?, selection: String?, selectionArgs: Array<out String>?): Int {
+        return 0
+    }
+
+    override fun delete(uri: Uri?, selection: String?, selectionArgs: Array<out String>?): Int {
+        return 0
+    }
+
+    override fun getType(uri: Uri?): String? {
+        return null
+    }
+}
