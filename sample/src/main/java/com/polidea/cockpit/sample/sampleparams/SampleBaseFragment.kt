2e4da75905e30be502b82b146a0c56c4d3e54a67
Wojciech Piwonski <wojciech.piwonski@polidea.com>
2018-08-08 18:15:44 +0200

    Info dialog added

--- sample/src/main/java/com/polidea/cockpit/sample/sampleparams/SampleBaseFragment.kt
+++ sample/src/main/java/com/polidea/cockpit/sample/sampleparams/SampleBaseFragment.kt
@@ -3,11 +3,13 @@ package com.polidea.cockpit.sample.sampleparams
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
+import android.support.v7.app.AlertDialog
 import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
+import com.polidea.cockpit.cockpit.Cockpit
 import com.polidea.cockpit.sample.R
 import com.polidea.cockpit.sample.Style
 import kotlinx.android.synthetic.main.fragment_shopping_cart.*
@@ -33,10 +35,10 @@ abstract class SampleBaseFragment<T : SampleBaseContract.Presenter> : Fragment()
         activity?.let {
             val window = it.window
             // clear FLAG_TRANSLUCENT_STATUS flag:
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
 
             // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
 
             // finally change the color
             window.statusBarColor = ContextCompat.getColor(it, statusBarColor)
@@ -66,10 +68,23 @@ abstract class SampleBaseFragment<T : SampleBaseContract.Presenter> : Fragment()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         presenter.start()
+        info.setOnClickListener { presenter.infoClicked() }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         presenter.stop()
+    }
+
+    override fun showInfoDialog() {
+        context?.let {
+            AlertDialog.Builder(it, Style.forValue(Cockpit.getStyleSelectedValue()).alertDialogStyleResId)
+                    .setTitle(R.string.info_title)
+                    .setMessage(R.string.info_message)
+                    .setPositiveButton(android.R.string.ok) { dialog, which ->
+                        dialog.dismiss()
+                    }
+                    .create().show()
+        }
     }
 }
