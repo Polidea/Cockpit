ad376634f940c11751d68cf413fd41ea63a08c7d
Marta Woldańska <marta.woldanska@polidea.com>
2018-08-08 15:35:33 +0200

    Cart item
    Cockpit version upgraded, build fixed
    
    Add and remove buttons added

---
+++ sample/src/main/java/com/polidea/cockpit/sample/shoppingcart/CartItem.kt
@@ -0,0 +1,28 @@
+package com.polidea.cockpit.sample.shoppingcart
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.polidea.cockpit.sample.R
+
+
+class CartItem() : Fragment() {
+    private var itemName: String = ""
+    private var itemPrice: Double = 0.0
+    private var imageResource: Int = R.drawable.shoes
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+            inflater.inflate(R.layout.cart_item, container, false)
+
+    companion object {
+        fun newInstance(itemName: String, itemPrice: Double, imageResource: Int): CartItem {
+            val instance = CartItem()
+            instance.itemName = itemName
+            instance.itemPrice = itemPrice
+            instance.imageResource = imageResource
+            return instance
+        }
+    }
+}
