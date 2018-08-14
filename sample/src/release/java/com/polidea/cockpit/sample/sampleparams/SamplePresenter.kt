e4ee16797643685aa9684b729b316aca685ac08e
Marta Woldańska <marta.woldanska@polidea.com>
2018-08-14 11:47:55 +0200

    Build fixed

--- sample/src/release/java/com/polidea/cockpit/sample/sampleparams/SamplePresenter.kt
+++ sample/src/release/java/com/polidea/cockpit/sample/sampleparams/SamplePresenter.kt
@@ -1,7 +1,9 @@
 package com.polidea.cockpit.sample.sampleparams
 
-class SamplePresenter(override val sampleView: SampleContract.View)
-    : SampleBasePresenter(sampleView), SampleContract.Presenter {
+import com.polidea.cockpit.sample.model.SampleModel
+
+class SamplePresenter(override val sampleView: SampleContract.View, override val sampleModel: SampleModel)
+    : SampleBasePresenter(sampleView, sampleModel), SampleContract.Presenter {
 
     init {
         sampleView.presenter = this
