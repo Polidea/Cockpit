db42939435f90756c3c535b27029c81221ea7177
Marcin Kornacki <marcin.kornacki@polidea.com>
2018-08-23 11:45:24 +0200

    Fix group margins

--- cockpit/src/main/java/com/polidea/cockpit/paramsedition/viewholder/GroupViewHolder.kt
+++ cockpit/src/main/java/com/polidea/cockpit/paramsedition/viewholder/GroupViewHolder.kt
@@ -7,9 +7,15 @@ import com.polidea.cockpit.R
 
 internal class GroupViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
 
-    val name: TextView = view.findViewById(R.id.cockpit_group_name)
+    private val separator: View = view.findViewById(R.id.cockpit_group_separator)
+    private val name: TextView = view.findViewById(R.id.cockpit_group_name)
 
     fun display(groupName: String?) {
+        if (adapterPosition == 0)
+            separator.visibility = View.GONE
+        else
+            separator.visibility = View.VISIBLE
+
         name.text = groupName ?: view.context.getString(R.string.default_group_name)
     }
 }
